ea475288593233d7d43c04a237bca3db2c199e63
Himeth Jayakody <himeth.jayakody@gmail.com>
2021-10-21 11:47:29 -0400

    Created an image slider
    Created an image slider to automatically go through each of the images.

--- itis3135/scripts/slideshow_script.js
+++ itis3135/scripts/slideshow_script.js
@@ -1,11 +1,30 @@
 $(function(){
-    //preload images
+    const pictures = [
+        ["images/hime_hime.jpg", "Himeth Jayakody"],
+        ["images/pineapple_fountain.jpg", "Pineapple Fountain @ Charleston, SC"],
+        ["images/jojo.jpg", "NC State Fair 2019"],
+        ["images/shivvy.jpg", "BabuQ with friends"],
+        ["images/dorothea_dix.jpg", "Dorothea Dix Park, June 2021"]
+    ];
+    //preload images taken from Image Swap application from Murach Slides
     $('#image-list div').each(function(){
         let swappedImage = new Image();
         swappedImage.src = $(this).children().attr('src');
+        //images.push(swappedImage.src);
+        // console.log(swappedImage.src.toString());
     });
+    
+    let counter = 0;
+    setInterval(function(){
+        counter++;
+        $('#main-image').attr('src', pictures[counter][0]);
+        $('#caption').text(pictures[counter][1]);
+        if(counter===4){
+            counter=-1;
+        }
+    }, 6500);
 
-    //set up event handlers for links
+    //set up event handlers for links taken from Image Swap application from Murach Slides
     $('#image-list div').click(function(evt){
         //swap image
         let imageURL = $(this).children().attr('src');
