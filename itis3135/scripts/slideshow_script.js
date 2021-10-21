a16daac14fbfdbd6fcc3e1312031a87f56ff8e8f
Himeth Jayakody <himeth.jayakody@gmail.com>
2021-10-21 12:34:14 -0400

    Reorganized code
    Moved the automatic image slider to another function and cleaned up the code a bit. Also validated HTML and CSS

--- itis3135/scripts/slideshow_script.js
+++ itis3135/scripts/slideshow_script.js
@@ -1,36 +1,24 @@
+const pictures = [
+    ["images/hime_hime.jpg", "Himeth Jayakody"],
+    ["images/pineapple_fountain.jpg", "Pineapple Fountain @ Charleston, SC"],
+    ["images/jojo.jpg", "NC State Fair 2019"],
+    ["images/shivvy.jpg", "BabuQ with friends"],
+    ["images/dorothea_dix.jpg", "Dorothea Dix Park, June 2021"]
+]; //array of pictures for the automatic image cycling function
+
 $(function(){
-    const pictures = [
-        ["images/hime_hime.jpg", "Himeth Jayakody"],
-        ["images/pineapple_fountain.jpg", "Pineapple Fountain @ Charleston, SC"],
-        ["images/jojo.jpg", "NC State Fair 2019"],
-        ["images/shivvy.jpg", "BabuQ with friends"],
-        ["images/dorothea_dix.jpg", "Dorothea Dix Park, June 2021"]
-    ];
     //preload images taken from Image Swap application from Murach Slides
     $('#image-list div').each(function(){
         let swappedImage = new Image();
         swappedImage.src = $(this).children().attr('src');
-        //images.push(swappedImage.src);
-        // console.log(swappedImage.src.toString());
     });
-    
-    let counter = 0;
-    setInterval(function(){
-        counter++;
-        $('#main-image').attr('src', pictures[counter][0]);
-        $('#caption').text(pictures[counter][1]);
-        if(counter===4){
-            counter=-1;
-        }
-    }, 6500);
 
     //set up event handlers for links taken from Image Swap application from Murach Slides
     $('#image-list div').click(function(evt){
+
         //swap image
         let imageURL = $(this).children().attr('src');
-        //$('#main-image').fadeOut('slow');
         $('#main-image').attr('src', imageURL);
-        //$('#main-image').fadeIn('slow');
 
         //swap caption
         let imageCaption = $(this).children().attr('alt');
@@ -40,3 +28,16 @@ $(function(){
         evt.preventDefault();
     });
 });
+
+$(function(){   
+    //automatically cylce through the different images
+    let counter = 0; //counter set at 0 since the first image is already displayed
+    setInterval(function(){ //Uses set interval so that each image stays for 6.5s
+        counter++;
+        $('#main-image').attr('src', pictures[counter][0]); //change main image to the next image in the array
+        $('#caption').text(pictures[counter][1]); //change caption to the next caption in the array
+        if(counter===4){
+            counter=-1; //sets counter to -1 to so that it starts at the first image
+        }
+    }, 6500);
+})
