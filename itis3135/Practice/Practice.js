5243401a04b8d06eb06331b1d780d24be68a334d
Himeth Jayakody <himeth.jayakody@gmail.com>
2021-10-23 00:38:31 -0400

    Update Practice.js
    Changed the price in items array to float and got rid of the parseFloats

--- itis3135/Practice/Practice.js
+++ itis3135/Practice/Practice.js
@@ -3,8 +3,8 @@ var $ = function(id) {
 };
 
 let items = [
-	{"name": "espresso", "price": "1.95", "details": "Delicious espresso. Wanna try it?"},
-	{"name": "cappuccino", "price": "3.45", "details": "Delicious Cappuccino!"}
+	{"name": "espresso", "price": 1.95, "details": "Delicious espresso. Wanna try it?"},
+	{"name": "cappuccino", "price": 3.45, "details": "Delicious Cappuccino!"}
 ];
 let order="";
 let total = 0.0;
@@ -16,13 +16,13 @@ window.onload = function() {
 	// for click event add item to order and update total
 	$('espresso').onclick = function(){
 		$('order').append(`${items[0].name} -- ${items[0].price} -- ${items[0].details}\n`);
-		total+=parseFloat(items[0].price);
+		total+=items[0].price;
 		$('total').innerHTML="Total: $"+total.toFixed(2);
 	};
 
 	$('cappuccino').onclick = function(){
 		$('order').append(`${items[1].name} -- ${items[1].price} -- ${items[1].details}\n`);
-		total+=parseFloat(items[1].price);
+		total+=items[1].price;
 		$('total').innerHTML="Total: $"+total.toFixed(2);
 	};
 
