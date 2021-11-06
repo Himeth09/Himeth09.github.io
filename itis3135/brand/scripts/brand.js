21b770258faa750ba5766281ad49a29f455ed142
Himeth Jayakody <himeth.jayakody@gmail.com>
2021-11-06 01:09:04 -0400

    Made customized validation button
    Replaced the validation links in the footer with buttons that validated each page

--- itis3135/brand/scripts/brand.js
+++ itis3135/brand/scripts/brand.js
@@ -33,3 +33,23 @@ function display() {
         document.getElementById('mobile').checked = false;
     }
 }
+
+function validateHTML(page){
+    if(page == "home"){
+        window.open("https://validator.w3.org/nu/?doc=https%3A%2F%2Fwebpages.uncc.edu%2Fhjayakod%2Fitis3135%2Fbrand%2F", "_blank");
+    }else if(page == "contact"){
+        window.open("https://validator.w3.org/nu/?doc=https%3A%2F%2Fwebpages.uncc.edu%2Fhjayakod%2Fitis3135%2Fbrand%2Fcontact.html", "_blank");
+    }else{
+        window.open("https://validator.w3.org/nu/?doc=https%3A%2F%2Fwebpages.uncc.edu%2Fhjayakod%2Fitis3135%2Fbrand%2Fservices.html", "_blank");
+    }
+}
+
+function validateCSS(page){
+    if(page == "home"){
+        window.open("https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwebpages.uncc.edu%2Fhjayakod%2Fitis3135%2Fbrand%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en", "_blank");
+    }else if(page == "contact"){
+        window.open("https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwebpages.uncc.edu%2Fhjayakod%2Fitis3135%2Fbrand%2Fcontact.html&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en", "_blank");
+    }else{
+        window.open("https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwebpages.uncc.edu%2Fhjayakod%2Fitis3135%2Fbrand%2Fservices.html&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en", "_blank");
+    }
+}
