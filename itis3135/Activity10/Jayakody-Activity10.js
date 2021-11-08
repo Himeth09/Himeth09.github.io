e4690c0b26db7dd98e257ab72cda383e0964cf53
Himeth Jayakody <himeth.jayakody@gmail.com>
2021-11-08 18:57:16 -0500

    Update Jayakody-Activity10.js
    Completed swapping images with animation

--- itis3135/Activity10/Jayakody-Activity10.js
+++ itis3135/Activity10/Jayakody-Activity10.js
@@ -3,59 +3,27 @@ $(document).ready(function() {
 	// preload the image for each link
 	$('#image-list a').each(function(){
 		let image = new Image();
-		image.src = $(this).children().attr('src');
+		image.src = $(this).attr('href');
 	});
 
 	// set up the event handlers for each link
 	$('#image_list a').click(function(evt){
 		
 		// get the image URL and caption for each image and animate the caption
-		// var nextImage = $('#image_list a img:first-child');
-		// console.log(nextImage.attr('src'));
-		// var nextCaption;
-		// var nextImageSource;
-		// setInterval(function (){
-		// 	$('#caption').fadeOut(3000);
-		// 	$('#image').fadeOut(3000, function(){
-		// 		if($('#image_list a img').next().length == 0){
-		// 			nextImage=('#image_list a img:first-child');
-		// 		}else{
-		// 			nextImage = $('#image_list a img').next();
-		// 		}
-		// 		nextImageSource = nextImage.src;
-		// 		nextCaption = nextImage.alt;
-		// 		$('#image').attr('src', nextImageSource).fadeIn(3000);
-		// 		$('#caption').text(nextCaption).fadeIn(3000);
-		// 	});
-		// });
-		let image, imageCounter=0, imageCache = [];
-		$('#image_list li a img').each(function(){
-			image = new Image();
-			image.src = $(this).attr('src');
-			image.title = $(this).attr('alt');
-			imageCache[imageCounter] = image;
-			imageCounter++;
-		});
+		let nextImage = $(this).attr('href');
+		let nextCaption = $(this).attr('title');
 
-		imageCounter = 0;
-		let nextImage;
-		setInterval(function(){
-			$('#caption').fadeOut(3000);
-			$('#image').fadeOut(3000, function(){
-				imageCounter = (imageCounter+1)%imageCache.length;
-				nextImage = imageCache[imageCounter];
-				$('#image').attr('src', nextImage.src).fadeIn(1000);
-				$('#caption').text(nextImage.title).fadeIn(1000);
-			}, 3000);
+		$('#caption').fadeOut(3000);
+		$('#image').fadeOut(3000, function(){
+			$('#image').attr('src', nextImage).fadeIn(3000);
+			$('#caption').text(nextCaption).fadeIn(3000);
+			$('#caption').animate({fontSize: '2.0em'},3000);
 		});
 
 		// cancel the default action of each link
 		evt.preventDefault();
 	});
 
-	
-	
-
 	// move the focus to the first link
-
+	$('li:first-child a').focus();
 }); // end ready
