4e5da6c3d68483acbc4890ccbc141fb4a9b2a284
Himeth Jayakody <himeth.jayakody@gmail.com>
2021-12-10 21:24:19 -0500

    Added ajax functionality to about
    Added ajax functionality so that the text in the accordion widget is retrieved from a json file

--- itis3135/client_project/scripts/about.js
+++ itis3135/client_project/scripts/about.js
@@ -1,4 +1,32 @@
+let filepath = "scripts/about.json";
 $(document).ready(function() {
+    let counter=1;
+    $.ajax({
+        type: "get",
+        url: filepath,
+        beforeSend: function() {
+            $("#section").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            // $(tabsSelector).html("");
+            $.each(data, function() {
+                $.each(this, function(key, value) {
+                    let tabsSelector = "#tabs-"+counter;
+                    $(tabsSelector).append(
+                        "<p>" + value.text + "</p>"
+                    );
+                    console.log(tabsSelector);
+                    counter++;
+                });
+            });
+        }
+    });
+
     $("#tabs").accordion({
         heightStyle: "content"
     });
